eaf9b8bd968ac5b609ccc887d9c42da08e7c49b3
A780 <a780a@yandex.ru>
2015-01-17 01:18:37 +0600

    Update translations

--- l10n/kinamania_en.ts
+++ l10n/kinamania_en.ts
@@ -105,14 +105,12 @@
         <translation type="obsolete">&lt;p&gt;&lt;strong&gt;Version 0.1&lt;/strong&gt;&lt;/p&gt;Keys: &lt;br&gt; Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt; F5 - New Game;&lt;br&gt; F8 - Reset;&lt;br&gt; F10 - Quit;&lt;br&gt; Pause or G - Pause;&lt;br&gt;&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/CherveMania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;p&gt;&lt;center&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;center&gt;&lt;b&gt;Anonymous artist and A780A, December 2014&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="43"/>
         <source>About Kinamania</source>
-        <translation>About Kinamania</translation>
+        <translation type="obsolete">About Kinamania</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="43"/>
         <source>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</source>
-        <translation>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</translation>
+        <translation type="obsolete">&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</translation>
     </message>
     <message>
         <source>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Anonymous artist and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</source>
@@ -130,36 +128,106 @@
 <context>
     <name>Widget</name>
     <message>
-        <location filename="../widget.cpp" line="387"/>
+        <location filename="../widget.cpp" line="402"/>
         <source>Pause</source>
         <translation>Pause</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="388"/>
+        <location filename="../widget.cpp" line="403"/>
         <source>Press any key to start the game!</source>
         <translation>Press any key to start the game!</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="391"/>
+        <location filename="../widget.cpp" line="406"/>
         <source>Kinamania - The Game</source>
         <translation>Kinamania - The Game</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="1394"/>
+        <location filename="../widget.cpp" line="1267"/>
+        <source>&lt;p&gt;&lt;strong&gt;Version 0.4&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the game is available on &lt;a href=&quot;https://vimeo.com/114717786&quot;&gt;Vimeo&lt;/a&gt;!&lt;br&gt;View Kinamania gameplay on &lt;a href=&quot;https://vimeo.com/114859939&quot;&gt;Vimeo&lt;/a&gt;.&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;&lt;a href=&quot;mailto:tsvr-kun@yandex.ru&quot;&gt;Tsveerkoon&lt;/a&gt; and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;</source>
+        <translation>&lt;p&gt;&lt;strong&gt;Version 0.4&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse or touch control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the game is available on &lt;a href=&quot;https://vimeo.com/114717786&quot;&gt;Vimeo&lt;/a&gt;!&lt;br&gt;View Kinamania gameplay on &lt;a href=&quot;https://vimeo.com/114859939&quot;&gt;Vimeo&lt;/a&gt;.&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;&lt;a href=&quot;mailto:tsvr-kun@yandex.ru&quot;&gt;Tsveerkoon&lt;/a&gt; and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1290"/>
+        <source>About Kinamania</source>
+        <translation>About Kinamania</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1503"/>
         <source>F5</source>
         <translation>F5</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="1395"/>
+        <location filename="../widget.cpp" line="1504"/>
         <source>F6</source>
         <translation>F6</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1560"/>
+        <source>Start</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1567"/>
+        <source>Game</source>
+        <translation>Game</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1572"/>
+        <source>Settings</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1577"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1588"/>
+        <source>Game Mode A</source>
+        <translation>Game Mode A</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1592"/>
+        <source>Game Mode B</source>
+        <translation>Game Mode B</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1596"/>
+        <source>Reset</source>
+        <translation>Reset</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1600"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1604"/>
+        <source>Sound</source>
+        <translation>Sound</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1610"/>
+        <source>Vibro</source>
+        <translation>Vibro</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1616"/>
+        <source>About...</source>
+        <translation>About...</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1620"/>
+        <source>About Qt...</source>
+        <translation>About Qt...</translation>
     </message>
     <message>
         <source>Press any key for start game!</source>
         <translation type="obsolete">Press any key for start game!</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="389"/>
+        <location filename="../widget.cpp" line="404"/>
         <source>Game over!</source>
         <translation>Game over!</translation>
     </message>
--- l10n/kinamania_ru.ts
+++ l10n/kinamania_ru.ts
@@ -105,14 +105,12 @@
         <translation type="obsolete">&lt;p&gt;&lt;strong&gt;Версия 0.1&lt;/strong&gt;&lt;/p&gt;Клавиши управления: &lt;br&gt; Q, A, P, L или 7, 9, 1, 3 на NumPad&apos;е - Движение;&lt;br&gt; F5 - Новая игра;&lt;br&gt; F8 - Сброс;&lt;br&gt; F10 - Выход;&lt;br&gt; Pause или G - Пауза;&lt;br&gt;&lt;br&gt;Исходный код доступен на &lt;a href=&quot;https://github.com/A780/CherveMania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;p&gt;&lt;center&gt;&lt;b&gt;Большое спасибо за помощь, /fag!&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;center&gt;&lt;b&gt;Художник-кун и A780A, Декабрь 2014&lt;/b&gt;&lt;/center&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="43"/>
         <source>About Kinamania</source>
-        <translation>О Кинамании</translation>
+        <translation type="obsolete">О Кинамании</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="43"/>
         <source>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</source>
-        <translation>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Версия 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Управление:&lt;/b&gt;&lt;br&gt;* Q, A, P, L или 7, 9, 1, 3 на NumPad&apos;е - Движение;&lt;br&gt;* F5 - Новая игра;&lt;br&gt;* F8 - Сброс;&lt;br&gt;* F10 - Выход;&lt;br&gt;* Pause или G - Пауза.&lt;br&gt;&lt;br&gt;Вы также можете управлять мышкой.&lt;br&gt;&lt;br&gt;Загрузить последнюю версию Кинамании можно &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;здесь&lt;/a&gt;.&lt;br&gt;Тизер игры Кинамания доступен на &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt; Исходный код доступен на &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;&apos;е.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Большое спасибо за помощь, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon и &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;Декабрь, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</translation>
+        <translation type="obsolete">&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Версия 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Управление:&lt;/b&gt;&lt;br&gt;* Q, A, P, L или 7, 9, 1, 3 на NumPad&apos;е - Движение;&lt;br&gt;* F5 - Новая игра;&lt;br&gt;* F8 - Сброс;&lt;br&gt;* F10 - Выход;&lt;br&gt;* Pause или G - Пауза.&lt;br&gt;&lt;br&gt;Вы также можете управлять мышкой.&lt;br&gt;&lt;br&gt;Загрузить последнюю версию Кинамании можно &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;здесь&lt;/a&gt;.&lt;br&gt;Тизер игры Кинамания доступен на &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt; Исходный код доступен на &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;&apos;е.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Большое спасибо за помощь, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Tsveerkoon и &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;Декабрь, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</translation>
     </message>
     <message>
         <source>&lt;p&gt;&lt;center&gt;&lt;img src=&quot;:/gfx/eyes.jpg&quot;/&gt;&lt;/center&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Version 0.3&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the Kinamania game is available on &lt;a href=&quot;http://www.youtube.com/watch?v=0ggWeAdZClc&quot;&gt;YouTube&lt;/a&gt;!&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;Anonymous artist and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;&lt;/td&gt;&lt;td&gt;&lt;img src=&quot;:/gfx/worm.png&quot;/&gt;&lt;/td&gt;&lt;/tr&gt;&lt;/table&gt;&lt;/center&gt;</source>
@@ -130,36 +128,106 @@
 <context>
     <name>Widget</name>
     <message>
-        <location filename="../widget.cpp" line="387"/>
+        <location filename="../widget.cpp" line="402"/>
         <source>Pause</source>
         <translation>Pause</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="388"/>
+        <location filename="../widget.cpp" line="403"/>
         <source>Press any key to start the game!</source>
         <translation>Нажмите клавишу для старта!</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="391"/>
+        <location filename="../widget.cpp" line="406"/>
         <source>Kinamania - The Game</source>
         <translation>Кинамания - Игра</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="1394"/>
+        <location filename="../widget.cpp" line="1267"/>
+        <source>&lt;p&gt;&lt;strong&gt;Version 0.4&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Key controls:&lt;/b&gt;&lt;br&gt;* Q, A, P, L or 7, 9, 1, 3 on NumPad - Move;&lt;br&gt;* F5 - New Game;&lt;br&gt;* F8 - Reset;&lt;br&gt;* F10 - Quit;&lt;br&gt;* Pause or G - Pause.&lt;br&gt;&lt;br&gt;Also, you can use mouse control.&lt;br&gt;&lt;br&gt;Get the latest release of the Kinamania game on &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;this page&lt;/a&gt;.&lt;br&gt;Teaser of the game is available on &lt;a href=&quot;https://vimeo.com/114717786&quot;&gt;Vimeo&lt;/a&gt;!&lt;br&gt;View Kinamania gameplay on &lt;a href=&quot;https://vimeo.com/114859939&quot;&gt;Vimeo&lt;/a&gt;.&lt;br&gt;Source code is available on &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Big thanks for Help, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;&lt;a href=&quot;mailto:tsvr-kun@yandex.ru&quot;&gt;Tsveerkoon&lt;/a&gt; and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;December, 2014&lt;/b&gt;&lt;br&gt;</source>
+        <translation>&lt;p&gt;&lt;strong&gt;Версия 0.4&lt;/strong&gt;&lt;/p&gt;&lt;b&gt;Клавиши управления:&lt;/b&gt;&lt;br&gt;* Q, A, P, L или 7, 9, 1, 3 на NumPad&apos;е - Движение;&lt;br&gt;* F5 - Новая игра;&lt;br&gt;* F8 - Сброс;&lt;br&gt;* F10 - Выход;&lt;br&gt;* Pause или G - Пауза.&lt;br&gt;&lt;br&gt;Также вы можете использовать управление мышкой или тачпадом.&lt;br&gt;&lt;br&gt;Скачать последнюю версию игры можно &lt;a href=&quot;https://github.com/A780/Kinamania/releases&quot;&gt;здесь&lt;/a&gt;.&lt;br&gt;Тизер Кинамании доступен на &lt;a href=&quot;https://vimeo.com/114717786&quot;&gt;Vimeo&lt;/a&gt;!&lt;br&gt;Ролик, демонстрирующий геймплей игры на &lt;a href=&quot;https://vimeo.com/114859939&quot;&gt;Vimeo&lt;/a&gt;.&lt;br&gt;Исходный код доступен на &lt;a href=&quot;https://github.com/A780/Kinamania&quot;&gt;GitHub&lt;/a&gt;&apos;е.&lt;br&gt;&lt;center&gt;&lt;table cellspacing=0 cellpadding=0&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;Большое спасибо за помощь, /fag!&lt;/b&gt;&lt;br&gt;&lt;b&gt;&lt;a href=&quot;mailto:tsvr-kun@yandex.ru&quot;&gt;Tsveerkoon&lt;/a&gt; and &lt;a href=&quot;mailto:a780a@yandex.ru&quot;&gt;A780&lt;/a&gt;&lt;/b&gt;&lt;br&gt;&lt;b&gt;Декабрь, 2014&lt;/b&gt;&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1290"/>
+        <source>About Kinamania</source>
+        <translation>О Кинамании</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1503"/>
         <source>F5</source>
         <translation>F5</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="1395"/>
+        <location filename="../widget.cpp" line="1504"/>
         <source>F6</source>
         <translation>F6</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1560"/>
+        <source>Start</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1567"/>
+        <source>Game</source>
+        <translation>Игра</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1572"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1577"/>
+        <source>Help</source>
+        <translation>Помощь</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1588"/>
+        <source>Game Mode A</source>
+        <translation>Игра А</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1592"/>
+        <source>Game Mode B</source>
+        <translation>Игра Б</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1596"/>
+        <source>Reset</source>
+        <translation>Сброс</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1600"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1604"/>
+        <source>Sound</source>
+        <translation>Звук</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1610"/>
+        <source>Vibro</source>
+        <translation>Вибро</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1616"/>
+        <source>About...</source>
+        <translation>О Кинамании...</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="1620"/>
+        <source>About Qt...</source>
+        <translation>О Qt...</translation>
     </message>
     <message>
         <source>Press any key for start game!</source>
         <translation type="obsolete">Нажмите клавишу для старта!</translation>
     </message>
     <message>
-        <location filename="../widget.cpp" line="389"/>
+        <location filename="../widget.cpp" line="404"/>
         <source>Game over!</source>
         <translation>Конец игры!</translation>
     </message>
